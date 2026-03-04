939d087b5c1f79c2d051b00aacbfd742ba56a7b3
Dung3t <tiendung3t#gmail.com>
2026-03-04 11:28:43 +0700

    update cloud request https

--- frontend/src/components/Chat/ChatWindow.jsx
+++ frontend/src/components/Chat/ChatWindow.jsx
@@ -7,7 +7,7 @@ export default function Content() {
 
   const sendAnswerAPI = async (text) => {
     try {
-      const response = await fetch("http://54.252.187.59:8000/chat/", {
+      const response = await fetch("https://54.252.187.59:80/chat/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
